cee07c8885a315c832fe5864f74b5a45cd52de8e
Lyhuwu <jeydon45@gmail.com>
2025-07-21 21:53:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -819,7 +819,7 @@ let galaxyAudio = null;
 
 function preloadGalaxyAudio() {
   const audioSources = [
-   "miCancion.mp3"
+   "miCancion1.mp3"
   ];
 
   const randomIndex = Math.floor(Math.random() * audioSources.length);
